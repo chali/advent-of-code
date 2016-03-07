09027501eba67412a1225bd9a98924adf4400ceb
Martin Chalupa <mchalupa@vendavo.com>
2016-03-06 21:05:59 -0800

    Proper scala getter

--- src/main/scala/cz/chali/advent/year2015/day17/Containers.scala
+++ src/main/scala/cz/chali/advent/year2015/day17/Containers.scala
@@ -2,16 +2,14 @@ package cz.chali.advent.year2015.day17
 
 import cz.chali.advent.year2015.input.InputReader
 
-class Container(capacity: Int) {
-    def getCapacity = capacity
-}
+class Container(val capacity: Int)
 
 object Containers {
 
     def findSolutionContainersCombinations(rawContainers: List[String], targetVolume: Int): IndexedSeq[List[Container]] = {
         val containers: List[Container] = rawContainers.map(c => new Container(c.toInt))
         val candidateCombinations = (1 to rawContainers.size).flatMap(containers.combinations)
-        candidateCombinations.filter(candidate => candidate.map(_.getCapacity).sum == targetVolume)
+        candidateCombinations.filter(candidate => candidate.map(_.capacity).sum == targetVolume)
     }
 
     def findSolutionWithMinimumContainersCombinations(rawContainers: List[String], targetVolume: Int): IndexedSeq[List[Container]] = {
